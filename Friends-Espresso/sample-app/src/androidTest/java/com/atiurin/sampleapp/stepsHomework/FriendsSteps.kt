347e6b3a3fe7c749df6069e9febe6535d3f27483
Hatake <ratishatirishvili55@gmail.com>
2024-01-25 01:42:45 +0400

    late

--- Friends-Espresso/sample-app/src/androidTest/java/com/atiurin/sampleapp/stepsHomework/FriendsSteps.kt
+++ Friends-Espresso/sample-app/src/androidTest/java/com/atiurin/sampleapp/stepsHomework/FriendsSteps.kt
@@ -1,14 +1,27 @@
 package com.atiurin.sampleapp.stepsHomework
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.atiurin.sampleapp.helper.isViewDisplayed
+import com.atiurin.sampleapp.helper.swiper
 import com.atiurin.sampleapp.helper.tap
 import com.atiurin.sampleapp.helper.typeText
-import com.atiurin.sampleapp.helper.waitForView
 import com.atiurin.sampleapp.pagesHomework.FriendsPage
+import com.atiurin.sampleapp.pagesHomework.FriendsPage.nameEmmet
+
 
 object FriendsSteps {
 
+    fun validationMainPage() {
+        with(FriendsPage) {
+            friendsTextMain.isViewDisplayed()
+        }
+    }
 
     fun openChat(){
         with(FriendsPage){
@@ -41,7 +54,27 @@ object FriendsSteps {
         }
     }
 
+    fun openEmmetChat(){
+        with(FriendsPage){
+            nameEmmet.tap(5)
+        }
+    }
 
+//    private fun swipeDown(){
+//        swiper(300, 100, 15)
+//    }
+//
+//    fun findFriend(){
+//        var i = 1
+//        while (nameEmmet.isViewDisplayed()){
+//            swipeDown()
+//            i++
+//        }
+//    }
+
+    fun findEmma(){
+        onView(withText("Emmet Brown")).perform(ViewActions.scrollTo()).check(ViewAssertions.matches(isDisplayed()));
+    }
 
 
 
--- Friends-Espresso/sample-app/src/androidTest/java/com/atiurin/sampleapp/tests/espresso/ChattingTests.kt
+++ Friends-Espresso/sample-app/src/androidTest/java/com/atiurin/sampleapp/tests/espresso/ChattingTests.kt
@@ -5,7 +5,10 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.atiurin.sampleapp.activity.MainActivity
 import com.atiurin.sampleapp.dataHomework.Data
+import com.atiurin.sampleapp.dataHomework.Data.screenText
+import com.atiurin.sampleapp.dataHomework.Data.sendText
 import com.atiurin.sampleapp.helper.isTextOnScreen
+import com.atiurin.sampleapp.helper.scrollSlowlyDown
 import com.atiurin.sampleapp.stepsHomework.FriendsSteps
 import com.atiurin.sampleapp.stepsHomework.MainManuSteps
 import org.junit.Rule
@@ -21,24 +24,36 @@ class ChattingTests {
 
     @Test
     fun testOne() {
-        isTextOnScreen(Data.screenText)
+        isTextOnScreen(screenText)
         Thread.sleep(5000) //ჩემთან სლიფების გარეშე ვერასწრებს აპი გახსნას და თქვენთან შეიძლება არ დასჭირდეს
         with(FriendsSteps) {
             openChat()
             checkChatIsCorrect()
-            enterText(Data.sendText)
-            checkEnteredText(Data.sendText)
+            enterText(sendText)
+            checkEnteredText(sendText)
             sendWrittenMessage()
         }
     }
 
     @Test
     fun TestTwo() {
-        isTextOnScreen(Data.screenText)
+        isTextOnScreen(screenText)
         Thread.sleep(5000)
         with(MainManuSteps) {
-            burgerManuTap()
         }
 
+    }
+
+    @Test
+    fun TestThree(){
+        with(FriendsSteps){
+            validationMainPage()
+            Thread.sleep(5000)
+            findEmma()
+            openEmmetChat()
+
+
+
+        }
     }
 }
